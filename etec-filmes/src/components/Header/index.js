32d6040fbb1a4b67d8248e3262c7f59045d2245d
Murilosgama <murilosgama@gmail.com>
2024-08-01 08:37:46 -0300

    salvo

--- etec-filmes/src/components/Header/index.js
+++ etec-filmes/src/components/Header/index.js
@@ -1,5 +1,11 @@
 function Header(){
     return(
-        
+        <header>
+            <span>ETECFLIX</span>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Assistir</a>
+            </nav>
+        </header>
     );
 }
